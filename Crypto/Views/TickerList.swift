a7fefbf3d1828f663c4795cf72f1fb44867357b6
Marcus Brissman <marcus.brissman@northernsquare.com>
2023-10-23 12:53:38 +0200

    Add button for switching currency

--- Crypto/Views/TickerList.swift
+++ Crypto/Views/TickerList.swift
@@ -10,15 +10,41 @@ import SwiftUI
 struct TickerList: View {
 
     @State var viewModel: TickerListModel
+    @State var selectedCurrency: Currency = .usd
 
     var body: some View {
         NavigationStack {
             List(viewModel.tickers) { ticker in
-                TickerRow(ticker: ticker, currency: .usd)
+                TickerRow(ticker: ticker, currency: selectedCurrency)
             }
             .navigationTitle("Currencies")
             .task {
                 await viewModel.refresh()
+            }
+            .toolbar {
+                ToolbarItem {
+                    currencyButton
+                }
+            }
+        }
+    }
+
+    private func toggleCurrency() {
+        withAnimation {
+            if selectedCurrency == .usd {
+                selectedCurrency = .sek
+            } else {
+                selectedCurrency = .usd
+            }
+        }
+    }
+
+    private var currencyButton: some View {
+        Button(action: toggleCurrency) {
+            if selectedCurrency == .usd {
+                Label("SEK", systemImage: "swedishkronasign.arrow.circlepath")
+            } else {
+                Label("USD", systemImage: "dollarsign.arrow.circlepath")
             }
         }
     }
