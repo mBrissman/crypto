9aa658d7fa8c96e2fa4eb9c7cc715435b6f21792
Marcus Brissman <marcus.brissman@northernsquare.com>
2023-11-03 16:03:18 +0100

    Return cached response if offline

--- Crypto/Networking/APISession.swift
+++ Crypto/Networking/APISession.swift
@@ -13,26 +13,66 @@ final class APISession {
     private let decoder = JSONDecoder()
 
     /// Retrieves crypto currency tickers and delivers the data asynchronously.
+    /// - Note: Returns cached tickers if not connected to internet.
     func tickers() async throws -> [Ticker] {
-        
+
         guard let url = URL(string: "https://api.coinlore.net/api/tickers/") else {
-            throw TickerStoreError.invalidURL
+            throw APISessionError.invalidURL
         }
 
-        // TODO: Handle http status codes
-        let (data, _) = try await session.data(from: url)
-        let response = try decoder.decode(TickersResponse.self, from: data)
+        let request = URLRequest(url: url)
+        let (data, response) = try await data(for: request)
 
-        return response.data
+        guard let response = response as? HTTPURLResponse else {
+            throw APISessionError.invalidResponse
+        }
+
+        guard response.statusCode == 200 else {
+            throw APISessionError.unexpectedStatusCode
+        }
+
+        let result = try decoder.decode(Result.self, from: data)
+        return result.data
+    }
+
+    /// Returns data for request if connected to internet, returns cached data otherwise.
+    private func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        guard session.configuration.waitsForConnectivity == false else {
+            return try cachedData(for: request)
+        }
+        do {
+            return try await session.data(for: request)
+        } catch let error as URLError where error.code == .notConnectedToInternet {
+            return try cachedData(for: request)
+        } catch {
+            throw error
+        }
+    }
+
+    /// Returns cached data for request from session cache.
+    private func cachedData(for request: URLRequest) throws -> (Data, URLResponse) {
+        guard let cache = session.configuration.urlCache else {
+            throw APISessionError.noCache
+        }
+
+        guard let cachedResponse = cache.cachedResponse(for: request) else {
+            throw APISessionError.noCachedResponse
+        }
+
+        return (cachedResponse.data, cachedResponse.response)
     }
 }
 
-enum TickerStoreError: Error {
+enum APISessionError: Error {
     case invalidURL
+    case invalidResponse
+    case unexpectedStatusCode
+    case noCache
+    case noCachedResponse
 }
 
 private extension APISession {
-    struct TickersResponse: Decodable {
+    struct Result: Decodable {
         let data: [Ticker]
     }
 }
